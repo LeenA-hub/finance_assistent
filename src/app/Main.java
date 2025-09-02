b4c37596d88ae674a76658adfae0c32e886baf08
leen128 <141294462+leen128@users.noreply.github.com>
2025-09-01 20:53:25 -0400

    doNE MAIN

---
+++ src/app/Main.java
@@ -0,0 +1,28 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import util.Database;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Database.createTables(); // Create DB tables if not exist
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+
+        // Optional: add CSS
+        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
+        stage.setTitle("Personal Finance Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
