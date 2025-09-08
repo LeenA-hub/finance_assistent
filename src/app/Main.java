08bf17b0791694a061daf7f5b059d666b4861177
LeenA-hub <almutileen7@gmail.com>
2025-09-07 22:17:52 -0400

    attempting again to relive this project

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,25 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import util.Database;
 
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Database.createTables(); // Create DB tables if not exist
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
-        Parent root = loader.load();
-        stage.setTitle("Personal Finance Manager");
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
-
-    // Optional: add CSS
-    scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
-
-        stage.setTitle("Personal Finance Manager");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+            Scene scene = new Scene(loader.load(), 900, 600);
+            primaryStage.setTitle("Personal Finance Manager");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
